c731b402297ec08619328e8525f3c61202c1567d
migonet <90347096+migonet@users.noreply.github.com>
2022-01-13 16:01:09 +0300

    Add files via upload

---
+++ lab2/Lab2/task2.cpp
@@ -0,0 +1,19 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double f(double x) {
+    return (sin(2 * x) + sin(5 * x) - sin(3 * x)) / (cos(x) - cos(3 * x) + cos(5 * x));
+}
+
+int main() {
+    double x;
+    while (true) {
+        cout << "Enter x: ";
+        cin >> x;
+        printf("f: %.4f\n", f(x));
+    }
+    return 0;
+}
